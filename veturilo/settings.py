629f2c2bad9858487d58f41e1ca0da6594d206f6
wrymaszewski <plantpathibb@gmail.comgit config --global user.email plantpathibb@gmail.comgit config --global user.email plantpathibb@gmail.com>
2018-05-19 20:28:40 +0200

    posgres fix

--- veturilo/settings.py
+++ veturilo/settings.py
@@ -76,17 +76,17 @@ WSGI_APPLICATION = 'veturilo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
